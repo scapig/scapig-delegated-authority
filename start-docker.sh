8b93ff81fffe3f8d1b331a06929a7300a87eed34
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-01-17 22:53:57 +0000

    update memory

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT=$(find . -type f -name scapig-delegated-authority)
 rm -f scapig-delegated-authority*/RUNNING_PID
-exec $SCRIPT -Dhttp.port=7030
+exec $SCRIPT -Dhttp.port=7030 -J-Xms128M -J-Xmx512m
